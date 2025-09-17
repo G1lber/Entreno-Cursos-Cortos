b2cd84e8e1710c2f4edcedd3b8f734c8d2c098ff
Maribel Fernanda Cortez Carvajal <maribel_cortez@soy.sena.edu.co>
2025-09-17 07:24:50 -0500

    views

--- curso/urls.py
+++ curso/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cursos/', views.crear_curso, name='cursos'),
 
 ]
--- curso/views.py
+++ curso/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'layout/index.html')
+
+def crear_curso(request):
+    return render(request, 'crear_curso.html')

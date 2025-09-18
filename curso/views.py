c5417ba79811dff1b66afb979ccdc45a1628fce3
Daniel Santiago Caicedo Rodriguez <daniels_caicedo@soy.sena.edu.co>
2025-09-18 09:07:54 -0500

    fix-instructorMenu

--- curso/views.py
+++ curso/views.py
@@ -196,7 +196,7 @@ def inicioSesion(request):
                 login(request, usuario)
                 messages.success(request, f"Inicio de sesión exitoso para {email}.")
                 if usuario.rol.nombre == "INSTRUCTOR":
-                    return redirect('generar_curso')  # Redirige a la página principal u otra página
+                    return redirect('dashboard')  # Redirige a la página principal u otra página
                 return redirect('viewUsuarios')  # Redirige a la página principal u otra página
             else:
                 messages.error(request, "Credenciales inválidas.")

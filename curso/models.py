80e593e4bc549949165267ce4c8dc4f387c2edb9
Jhodaqui <quirajh@gmail.com>
2025-09-18 12:02:00 -0500

    migracion de estados

--- curso/models.py
+++ curso/models.py
@@ -77,21 +77,37 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return f"{self.email} - {self.documento}"
+    
+class Estado(models.Model):
+    id = models.AutoField(primary_key=True)  
+    estado = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.estado
+
 class Curso(models.Model):
     programa = models.ForeignKey(
-        Programa,
+        "Programa",
         on_delete=models.CASCADE,
         related_name="cursos"
     )
     usuario = models.ForeignKey(
-        Usuario,
+        "Usuario",
         on_delete=models.CASCADE,
         related_name="cursos"
     )
     fecha_inicio = models.DateTimeField()
     fecha_fin = models.DateTimeField()
     max_inscripciones = models.IntegerField(default=25)
-    estado = models.IntegerField(null=True, blank=True)
+
+    estado = models.ForeignKey(
+        Estado,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="cursos"
+    )
+
     caracterizacion = models.CharField(max_length=50, null=True, blank=True)
     carta = models.CharField(max_length=50, null=True, blank=True)
     pdf_documentos = models.CharField(max_length=50, null=True, blank=True)

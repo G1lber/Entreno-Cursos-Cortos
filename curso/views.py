015b60f9cb7ee4ed2b2f3ff9a7d0fa5e4935c680
Maribel Fernanda Cortez Carvajal <maribel_cortez@soy.sena.edu.co>
2025-09-19 08:35:58 -0500

    Revert "Merge branch 'fer'"
    This reverts commit 2ae21c268949418f30fd6d2894b791b533107e59, reversing
    changes made to 035ba3ff51dd6eb8f570d28853372092854247d3.

--- curso/views.py
+++ curso/views.py
@@ -348,6 +348,7 @@ def registrar_aspirante(request, curso_id):
 # Ruta al ejecutable de Tesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
+
 @csrf_exempt
 def ocr_aspirante(request):
     if request.method == "POST" and request.FILES.get("archivo_documento"):
